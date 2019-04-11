3d60400a4fbdb7402686da5f03c2e724a4e05d87
Kirill Savchenko <k.savchenko@devrain.com>
2019-04-11 12:12:40 +0300

    Removing files

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=conversationhistorysample-Web-Deploy.pubxml -p:Password=3dMxq1nJclq1sjFLCAKqjqPCFtWbLrL3sXsoJrq36WojFiBQ0fvmzcsKoDQ4
-
